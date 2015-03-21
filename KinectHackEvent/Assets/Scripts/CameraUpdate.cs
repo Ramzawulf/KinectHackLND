3bc28ca65d8fb63230fc335de73d4d2b0e81613c
YoHann1992 <yohann.advikolanu@gmail.com>
2015-03-21 16:48:01 +0000

    ClientScene

---
+++ KinectHackEvent/Assets/Scripts/CameraUpdate.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraUpdate : MonoBehaviour 
+{
+    private GameObject FirstPersonCamera;
+    private GameObject Head;
+
+	// Use this for initialization
+	void Start () 
+    {
+        FirstPersonCamera = GameObject.Find("FirstPersonCamera");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (Head == null)
+        {
+            Head = GameObject.Find("Head");
+        }
+
+        if(Head != null)
+        {
+            FirstPersonCamera.transform.position = Head.transform.position;
+            FirstPersonCamera.transform.rotation = Head.transform.rotation;
+        }
+	}
+}
