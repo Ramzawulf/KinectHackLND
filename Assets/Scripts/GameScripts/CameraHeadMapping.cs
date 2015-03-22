54cfece79a95cb52176c0c9ed888604d7e011456
Ramzawulf <rene.zurita.sanchez@gmail.com>
2015-03-22 01:10:16 +0000

    New Client Scene integration

---
+++ Assets/Scripts/GameScripts/CameraHeadMapping.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraHeadMapping : MonoBehaviour 
+{
+    private GameObject FirstPersonCamera;
+    private GameObject Head;
+
+	// Use this for initialization
+	void Start () 
+    {
+		FirstPersonCamera = GameObject.Find("Main Camera");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (Head == null)
+        {
+			Head = GameObject.Find("joint_Head");
+        }
+
+        if(Head != null)
+        {
+			FirstPersonCamera.transform.position = Head.transform.position;
+            //FirstPersonCamera.transform.rotation = Head.transform.rotation;
+        }
+	}
+}
---
+++ Assets/Scripts/GameScripts/Moves.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Windows.Kinect;
+
+public class Moves : MonoBehaviour 
+{
+	private GameObject HandLeft;
+	private GameObject HandRight;
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (HandLeft == null && HandRight == null)
+        {
+			HandLeft = GameObject.Find("joint_HandLT");
+			HandRight = GameObject.Find("joint_HandRT");
+        }
+
+		if(HandLeft != null && HandRight != null)
+        {
+			CheckForMove();
+        }
+
+	}
+
+
+	void CheckForMove()
+	{
+		if((HandRight.transform.position - HandLeft.transform.position).sqrMagnitude < 0.1f)
+		{
+			Debug.Log ("Kameha-meha!");
+		}
+	}
+
+}
