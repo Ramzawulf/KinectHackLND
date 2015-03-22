30d3de3424b026b22c2cc2166af22f4f8afc3936
Ramzawulf <rene.zurita.sanchez@gmail.com>
2015-03-22 12:47:39 +0000

    2 Player Spawn

---
+++ Assets/Scripts/Server Side/NetworkManager.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class NetworkManager : MonoBehaviour {
+
+    public int port = 25001;
+    private string defaultIP = "127.0.0.1";
+
+
+	public string typeName = "Dojo";
+	public string gameName = "Kinect Fight Game";
+    public string connectionIP;
+    public Text statusLabel;
+    public Text ipField;
+    public InputField iField;
+
+    public Transform PlayerOnePrefab;
+    public Transform PlayerTwoPrefab;
+	public KinectManager knctManagerPrefab;
+	public Transform P1SpawnPoint;
+	public Transform P2SpawnPoint;
+	private List<GameObject> players;
+
+	void Awake(){
+		players = new List<GameObject> ();
+	}
+
+	void Update () {
+			UpdateStatusLabel ();
+	}
+	
+	void OnConnectedToServer()
+	{
+		SpawnPlayer();
+	}
+	
+	private void SpawnPlayer()
+	{
+		if (players.Count == 0) {
+			//Spawn P1
+			players.Add (Network.Instantiate (PlayerOnePrefab, P1SpawnPoint.position, Quaternion.identity, 0) as GameObject);
+				Network.Instantiate(knctManagerPrefab, new Vector3(0,0,0), Quaternion.identity, 99);
+		} else if (players.Count == 1) {
+			players.Add (Network.Instantiate (PlayerOnePrefab, P2SpawnPoint.position, Quaternion.identity, 0) as GameObject);
+		}
+	}
+
+    public void StartServer()
+    {
+		Network.InitializeServer(2, port, !Network.HavePublicAddress());
+	}
+
+    public void StopServer()
+    {
+        if (Network.peerType == NetworkPeerType.Server)
+        {
+          Network.Disconnect(200);
+        }
+    }
+
+    public void ConnectAsClient()
+    {
+        if (iField == null || iField.text == null)
+        {
+            connectionIP = defaultIP;
+        }
+        else
+        {
+            connectionIP = iField.text;
+        }
+
+        Network.Connect(connectionIP, port);
+    }
+	
+	private void UpdateStatusLabel(){
+		if (Network.peerType == NetworkPeerType.Disconnected) {
+			statusLabel.text = "Disconnected";
+		}
+		else if (Network.peerType == NetworkPeerType.Client)
+		{
+			statusLabel.text = "Connected as client.";
+		}else if (Network.peerType == NetworkPeerType.Server){
+			
+			statusLabel.text = "Connected as server";
+		}
+	}
+}
--- Assets/Scripts/Server Side/TestCube.cs
+++ Assets/Scripts/Server Side/TestCube.cs
@@ -3,19 +3,48 @@ using System.Collections;
 
 public class TestCube : MonoBehaviour
 {
-	void OnSerializeNetworkView(BitStream stream, NetworkMessageInfo info)
+	public float speed = 10f;
+	public Rigidbody rigidbody;
+	private NetworkView nwView;
+
+	void Awake(){
+		rigidbody = gameObject.GetComponent<Rigidbody> ();
+		nwView = GetComponent<NetworkView> ();
+	}
+	
+	void Update()
 	{
+		if (nwView.isMine) {
+			InputMovement ();
+		}
+	}
+
+	void InputMovement()
+	{
+		if (Input.GetKey(KeyCode.W))
+			rigidbody.MovePosition(rigidbody.position + Vector3.forward * speed * Time.deltaTime);
+		
+		if (Input.GetKey(KeyCode.S))
+			rigidbody.MovePosition(rigidbody.position - Vector3.forward * speed * Time.deltaTime);
+		
+		if (Input.GetKey(KeyCode.D))
+			rigidbody.MovePosition(rigidbody.position + Vector3.right * speed * Time.deltaTime);
+		
+		if (Input.GetKey(KeyCode.A))
+			rigidbody.MovePosition(rigidbody.position - Vector3.right * speed * Time.deltaTime);
+	}
+
+	void OnSerializeNetworkView(BitStream stream, NetworkMessageInfo info){
+		Vector3 syncPosition = Vector3.zero;
 		if (stream.isWriting)
 		{
-			Vector3 myPosition = transform.position;
-			stream.Serialize(ref myPosition);
+			syncPosition = rigidbody.position;
+			stream.Serialize(ref syncPosition);
 		}
 		else
 		{
-			Vector3 receivedPosition = Vector3.zero;
-			stream.Serialize(ref receivedPosition); //"Decode" it and receive it
-			transform.position = receivedPosition;
+			stream.Serialize(ref syncPosition);
+			rigidbody.position = syncPosition;
 		}
 	}
 }
- 
